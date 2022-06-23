f1994d5f6e6d7c2577bad49c0c5e7c66dc227b76
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-23 20:20:15 +0530

    Add functions in database for crud in database

--- pkg/models/medicine.go
+++ pkg/models/medicine.go
@@ -27,3 +27,31 @@ func init() {
 	// Auto migrate with empty Med
 	db.AutoMigrate(&Med{})
 }
+
+// Function to create Med
+func (m *Med) CreateMed () *Med {
+	db.NewRecord(m)
+	db.Create(&m)
+	return m
+}
+
+// Function to get med
+func GetMed() []Med {
+	var Medicines []Med
+	db.Find(&Medicines)
+	return Medicines
+}
+
+// Function to get med by id
+func GetMedById (Id int64) (*Med, *gorm.DB){
+	var getMed Med
+	db := db.Where("ID=?", Id).Find(&getMed)
+	return &getMed, db
+}
+
+// Function to delete med by id
+func DeleteMed (ID int64) Med {
+	var med Med
+	db.Where("ID=?", ID).Delete(med)
+	return med
+}
