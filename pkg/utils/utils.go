9fc7c5d6cf624ae32441ef26baaf8ff2ab0d9bb9
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-23 19:34:13 +0530

    Add utils file which parse json body to our useful struct

---
+++ pkg/utils/utils.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+// This file is mainly to parse the JSON body
+
+func ParseBody (r *http.Request, x interface {}) {
+	// Body will be read and pass to the body
+	if body,err := ioutil.ReadAll(r.Body); err == nil {
+		// Now unmarshal will parse the byte data to our desired struct and pass to x
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return
+		}
+	}
+}
