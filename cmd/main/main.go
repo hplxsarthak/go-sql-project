6174a793dcb8a3ae432ebfc28438ae7c65b210d9
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-24 11:39:11 +0530

    Add cors middleware to remove cors warning in frontend

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,12 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/hplxsarthak/go-sql-project/pkg/routes"
+	"github.com/gin-contrib/cors"
 
 )
 
 func main() {
 	// Create a Server
 	r := gin.Default()
+
+	r.Use(cors.Default())
 	routes.RegisterMedRoutes(r)
 
 	// Start the Server
